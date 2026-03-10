61fe7c5b607305e530206e582f176844a2ff91be
nhutruong47 <truongthainhuu@gmail.com>
2026-03-10 08:00:48 +0700

    Fix IDE errors: remove extra spaces in Thymeleaf expressions, remove unused imports

--- src/main/java/com/hsf/hotel/controller/BookingController.java
+++ src/main/java/com/hsf/hotel/controller/BookingController.java
@@ -9,7 +9,6 @@ import com.hsf.hotel.service.BookingService;
 import com.hsf.hotel.service.ReviewService;
 import com.hsf.hotel.service.RoomService;
 import com.hsf.hotel.service.VoucherService;
-import com.hsf.hotel.model.Voucher;
 import java.math.BigDecimal;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
